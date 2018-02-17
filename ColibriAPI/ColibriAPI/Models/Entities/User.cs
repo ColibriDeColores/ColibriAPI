bc31d4bc325841ea4c13ee844b5d097bcd25d46c
Marco Dominguez <marco.3292@gmail.com>
2018-02-17 11:37:37 -0600

    Add user entity and createUser mapping model.

---
+++ ColibriAPI/ColibriAPI/Models/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using ColibriAPI.Entities;
+using System;
+
+namespace ColibriAPI.Models.Entities
+{
+    public class User : Entity
+    {
+        public User()
+        {
+            //Entity Framework Core Only
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
