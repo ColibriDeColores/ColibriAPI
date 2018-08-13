bc54d9c36eb75a1c4bfc9c17f6f14fa82ff39855
Marco Dominguez <marco.3292@gmail.com>
2018-08-13 17:40:06 -0500

    Modify existing User entity.

--- ColibriAPI/ColibriAPI/Models/Entities/User.cs
+++ ColibriAPI/ColibriAPI/Models/Entities/User.cs
@@ -10,8 +10,15 @@ namespace ColibriAPI.Models.Entities
             //Entity Framework Core Only
         }
 
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
+        public Guid PasswordSalt { get; set; }
         public DateTime Birthday { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsOrganizer { get; set; }
+        public bool IsPlayer { get; set; }
     }
 }
