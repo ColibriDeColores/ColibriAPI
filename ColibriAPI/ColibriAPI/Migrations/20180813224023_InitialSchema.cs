588b35110e42b84aae2c899c56ad8cf82c257755
Marco Dominguez <marco.3292@gmail.com>
2018-08-13 17:40:48 -0500

    Add Initial Schema migration.

---
+++ ColibriAPI/ColibriAPI/Migrations/20180813224023_InitialSchema.cs
@@ -0,0 +1,276 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ColibriAPI.Migrations
+{
+    public partial class InitialSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedByUser = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTimeOffset>(nullable: false),
+                    DateDeleted = table.Column<DateTimeOffset>(nullable: true),
+                    DateUpdated = table.Column<DateTimeOffset>(nullable: true),
+                    DeletedByUser = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    PlayDate = table.Column<DateTime>(nullable: false),
+                    TournamentStage = table.Column<int>(nullable: false),
+                    UpdatedByUser = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedByUser = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTimeOffset>(nullable: false),
+                    DateDeleted = table.Column<DateTimeOffset>(nullable: true),
+                    DateUpdated = table.Column<DateTimeOffset>(nullable: true),
+                    DeletedByUser = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    UpdatedByUser = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tournaments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedByUser = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTimeOffset>(nullable: false),
+                    DateDeleted = table.Column<DateTimeOffset>(nullable: true),
+                    DateUpdated = table.Column<DateTimeOffset>(nullable: true),
+                    DeletedByUser = table.Column<string>(nullable: true),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    UpdatedByUser = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tournaments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    CreatedByUser = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTimeOffset>(nullable: false),
+                    DateDeleted = table.Column<DateTimeOffset>(nullable: true),
+                    DateUpdated = table.Column<DateTimeOffset>(nullable: true),
+                    DeletedByUser = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    IsAdmin = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    IsOrganizer = table.Column<bool>(nullable: false),
+                    IsPlayer = table.Column<bool>(nullable: false),
+                    LastName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    PasswordSalt = table.Column<Guid>(nullable: false),
+                    UpdatedByUser = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Entries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedByUser = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTimeOffset>(nullable: false),
+                    DateDeleted = table.Column<DateTimeOffset>(nullable: true),
+                    DateUpdated = table.Column<DateTimeOffset>(nullable: true),
+                    DeletedByUser = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    TeamId = table.Column<Guid>(nullable: true),
+                    TournamentId = table.Column<Guid>(nullable: true),
+                    UpdatedByUser = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Entries_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Entries_Tournaments_TournamentId",
+                        column: x => x.TournamentId,
+                        principalTable: "Tournaments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rosters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedByUser = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTimeOffset>(nullable: false),
+                    DateDeleted = table.Column<DateTimeOffset>(nullable: true),
+                    DateUpdated = table.Column<DateTimeOffset>(nullable: true),
+                    DeletedByUser = table.Column<string>(nullable: true),
+                    GameId = table.Column<Guid>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    TeamId = table.Column<Guid>(nullable: true),
+                    TournamentId = table.Column<Guid>(nullable: true),
+                    UpdatedByUser = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rosters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rosters_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rosters_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rosters_Tournaments_TournamentId",
+                        column: x => x.TournamentId,
+                        principalTable: "Tournaments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedByUser = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTimeOffset>(nullable: false),
+                    DateDeleted = table.Column<DateTimeOffset>(nullable: true),
+                    DateUpdated = table.Column<DateTimeOffset>(nullable: true),
+                    DeletedByUser = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    PlayerIdentifier = table.Column<string>(nullable: true),
+                    PlayerTag = table.Column<string>(nullable: true),
+                    RosterId = table.Column<Guid>(nullable: true),
+                    TeamId = table.Column<Guid>(nullable: true),
+                    UpdatedByUser = table.Column<string>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Players_Rosters_RosterId",
+                        column: x => x.RosterId,
+                        principalTable: "Rosters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Players_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Players_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entries_TeamId",
+                table: "Entries",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entries_TournamentId",
+                table: "Entries",
+                column: "TournamentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_RosterId",
+                table: "Players",
+                column: "RosterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TeamId",
+                table: "Players",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_UserId",
+                table: "Players",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rosters_GameId",
+                table: "Rosters",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rosters_TeamId",
+                table: "Rosters",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rosters_TournamentId",
+                table: "Rosters",
+                column: "TournamentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entries");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Rosters");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+
+            migrationBuilder.DropTable(
+                name: "Tournaments");
+        }
+    }
+}
