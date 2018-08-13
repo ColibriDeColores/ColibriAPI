0b5802c5d6fd9107de8fb854b47d44556ad3a41d
Marco Dominguez <marco.3292@gmail.com>
2018-08-13 17:40:14 -0500

    Modify context.

--- ColibriAPI/ColibriAPI/DataAccess/ColibriApiContext.cs
+++ ColibriAPI/ColibriAPI/DataAccess/ColibriApiContext.cs
@@ -8,6 +8,12 @@ namespace ColibriAPI.DataAccess
             : base(options)
         { }
 
+        public DbSet<Models.Entities.Entry> Entries { get; set; }
+        public DbSet<Models.Entities.Game> Games { get; set; }
+        public DbSet<Models.Entities.Player> Players { get; set; }
+        public DbSet<Models.Entities.Roster> Rosters { get; set; }
+        public DbSet<Models.Entities.Team> Teams { get; set; }
+        public DbSet<Models.Entities.Tournament> Tournaments { get; set; }
         public DbSet<Models.Entities.User> Users { get; set; }
     }
 }
